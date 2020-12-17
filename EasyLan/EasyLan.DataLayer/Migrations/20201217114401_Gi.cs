1b0eba33e75c6b676e0ade4639278c57852fad53
Роман <noctuathene@yandex.ru>
2020-12-17 17:16:43 +0500

    admin request form

---
+++ EasyLan/EasyLan.DataLayer/Migrations/20201217114401_Gi.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyLan.DataLayer.Migrations
+{
+    public partial class Gi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "ClubRequests",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "ClubRequests",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "ClubRequests");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "ClubRequests");
+        }
+    }
+}
--- EasyLan/EasyLan.Web/Models/ClubFeedbackViewModel.cs
+++ EasyLan/EasyLan.Web/Models/ClubFeedbackViewModel.cs
@@ -10,5 +10,9 @@ namespace EasyLan.Web.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
     }
 }
