f476d0462db6fc645c27216d75298887c8403af2
Andreyinthesky <and19980316@gmail.com>
2020-12-18 14:41:26 +0500

    club form на всякий

---
+++ EasyLan/EasyLan-front/src/ui/components/ForClubForm/ForClubForm.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+interface IForClubFormProps {
+}
+
+type ForClubFormValues = {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+// const formInitialValues: ForClubFormValues = {
+// };
+
+const ForClubForm: React.FunctionComponent<IForClubFormProps> = (props) => {
+  return null;
+};
+
+export default ForClubForm;
